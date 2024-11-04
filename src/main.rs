8831a9fee1f7df53d916822509be9813d2cc2e9d
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-11-04 16:30:23 -0500

    Enable Windows subsystem in release builds
    This uncommenting of the cfg_attr directive activates the Windows subsystem for non-debug builds. It ensures a smoother user experience by preventing a console window from appearing in the release version on Windows.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use chrono::Duration;
 use std::ffi::OsStr;
 use std::os::windows::ffi::OsStrExt;
